841bfb855fb257f3c28198923be73242aed7e549
mrcruel <mrcrueloneman@gmail.com>
2024-01-15 19:46:37 +0300

    before my works

--- src/styles/Global.Styled.ts
+++ src/styles/Global.Styled.ts
@@ -16,7 +16,8 @@ export const GlobalStyle = createGlobalStyle`
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        color: ${theme.colors.font}
+        color: ${theme.colors.font};
+        line-height: 1.2;
     }
     
     a {
@@ -30,6 +31,10 @@ export const GlobalStyle = createGlobalStyle`
     button {
         background-color: unset;
         border: none;
+    }
+    
+    section {
+        padding: 100px 0;
     }
     
     section:nth-of-type(odd) {
